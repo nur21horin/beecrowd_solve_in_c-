db4f755df4566758b1c33ea3e3e7ac4245359d18
NUR MOHAMMMOD <143373468+nur21horin@users.noreply.github.com>
2024-05-06 12:14:42 +0600

    Add files via upload

---
+++ 1026bee.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+/*Nur mohammod
+Uri 1026 solution*/
+using namespace std;
+int main(){
+    unsigned long long a,b;
+
+    while(cin>>a>>b){
+    cout<<(a^b)<<endl;}
+    return 0;
+}
